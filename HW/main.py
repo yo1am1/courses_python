a0766fb1860cbab20ed4f92f436a3576bd241430
Yo1 <bigdiebam@gmail.com>
2023-04-13 20:15:47 +0300

    ,

--- HW/main.py
+++ HW/main.py
@@ -5,9 +5,6 @@ def parse(query: str) -> dict:
     return {}
 
 
-
-
-
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
